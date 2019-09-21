6af8e83964853274f59b899b0cad9c1fd7cd76bd
Tomer Weller <tomer.weller@gmail.com>
2019-09-21 11:03:20 -0700

    add mako config

--- .config/sway/idle/run.sh
+++ .config/sway/idle/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIM_TIMEOUT=60 # one minute
+DIM_TIMEOUT=120 # one minute
 let LOCK_TIMEOUT=$DIM_TIMEOUT+60
 let DPMS_TIMEOUT=$LOCK_TIMEOUT+60
 let SUSPEND_TIMEOUT=$DPMS_TIMEOUT+60
@@ -13,4 +13,5 @@ swayidle \
     timeout $LOCK_TIMEOUT   "$IDLE/lock.sh" \
     timeout $DPMS_TIMEOUT   "$IDLE/outputs_off.sh" \
         resume "$IDLE/outputs_on.sh" \
-    timeout $SUSPEND_TIMEOUT "$IDLE/suspend.sh"
+    timeout $SUSPEND_TIMEOUT "$IDLE/suspend.sh" \
+    before-sleep "$IDLE/lock.sh"
